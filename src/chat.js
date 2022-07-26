bd4a2ffc93a308acb63ff425845191b00eae8c9b
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2022-07-26 14:20:44 +0300

    Add submit method.

--- src/chat.js
+++ src/chat.js
@@ -1,1 +1,13 @@
+let url = 'ws://localhost:8080/ws';
+let ws = new WebSocket(url);
+
+ws.onclose = event => console.log(`Closed ${event.code}`);
+
+document.forms.sendmessage.onsubmit = function() {
+    let outMessage = this.message.value;
+    ws.send(outMessage);
+    ws.close();
+    return false;
+  };
+
 console.log('Work!');
