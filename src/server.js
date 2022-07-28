cc38fcf2125146172324f451cda1c99e0a546d8e
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2022-07-29 01:20:37 +0300

    Sharing chat from server.

--- src/server.js
+++ src/server.js
@@ -6,7 +6,17 @@ const wsServer = new ws.Server({ noServer: true });
 const visitors = new Set();
 
 http.createServer((req, res) => {
-  wsServer.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
+  console.log(req.url);
+  if (req.url == '/ws' && req.headers.upgrade &&
+    req.headers.upgrade.toLowerCase() == 'websocket' && req.headers.connection.match(/\bupgrade\b/i)) {
+    wsServer.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
+    } else if (req.url == '/') {
+      fs.createReadStream('./src/index.html').pipe(res);
+    } else if (req.url == '/style.css') {
+      fs.createReadStream('./src/style.css').pipe(res);
+    } else if (req.url == '/chat.js') {
+      fs.createReadStream('./src/chat.js').pipe(res);
+    }
 }).listen(8080);
 
 function onSocketConnect(ws) {
