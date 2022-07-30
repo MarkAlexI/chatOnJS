2f9f2696d1148b68b68d9f2688da227487e3b181
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2022-07-30 08:54:03 +0300

    Add random generation userId.

--- src/chat.js
+++ src/chat.js
@@ -1,11 +1,16 @@
 let url = 'ws://localhost:8080/ws';
 let ws = new WebSocket(url);
+let id = 'user'.concat(~~(Math.random() * 1e6));
 
 ws.onclose = event => console.log(`Closed ${event.code}`);
 
 document.forms.sendmessage.onsubmit = function() {
-    let messageOut = this.message.value;
-    ws.send(messageOut);
+    let messageOut = {
+      text: this.message.value,
+      id: id,
+      date: Date.now(),
+    };
+    ws.send(JSON.stringify(messageOut));
     return false;
   };
 
@@ -21,14 +26,20 @@ ws.onmessage = function(event) {
 };
 
 function displayMessage(message) {
+  message = JSON.parse(message);
+  
   let messageDiv = document.createElement('div');
   messageDiv.classList.add('incoming');
-  messageDiv.textContent = message;
+  messageDiv.textContent = message.text;
   document.getElementById('chat').prepend(messageDiv);
   
-  let date = new Date();
+  let userId = document.createElement('p');
+  userId.classList.add('user');
+  userId.textContent = message.id;
+  document.getElementsByClassName('incoming')[0].prepend(userId);
+  
   let timePoint = document.createElement('p');
   timePoint.classList.add('time');
-  timePoint.textContent = date.toTimeString().slice(0, 8);
+  timePoint.textContent = new Date(message.date).toTimeString().slice(0, 8);
   document.getElementsByClassName('incoming')[0].prepend(timePoint);
 }
