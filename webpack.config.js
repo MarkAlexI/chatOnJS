0dd72938ee491828c1cadf9e90f6088c007551fc
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2022-08-29 08:22:46 +0300

    Add html-webpack-plugin to project.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
     index: './src/chat.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Chat',
+    }),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
